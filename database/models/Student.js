707052f03ada04b2e25abf8d4393f703d86e87d7
TahminaM <Tahminamunni07@gmail.com>
2022-05-15 00:15:43 -0400

    sequelize models modified to fit requirements

--- database/models/Student.js
+++ database/models/Student.js
@@ -33,15 +33,15 @@ const Student = db.define("student", {
 
   imageUrl: {
     type: Sequelize.STRING,
-    defaultValue: ""
+    defaultValue: "https://64.media.tumblr.com/4c62bf7c01d15c7655adeb8a36b9702d/e7531ecdce802bee-c9/s640x960/838eaa8ff68dc51c2b2a38a088767a9704130d58.jpg"
   },
 
   gpa: {
     type: Sequelize.DECIMAL,
     defaultValue: 0,
     validate: {
-        min: 0,
-        max: 4
+        min: 0.0,
+        max: 4.0
     }
   }
 });
